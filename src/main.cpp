f4261219c699af854e8bbc0e2546b9bcee464451
Valentin Werner <vwerner1@stud.hs-offenburg.de>
2024-12-04 18:32:42 +0100

    Getting time and printing it to serial is now possible

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,99 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-#define  LED 2
+#include "wifiPassword.h"
+
+#define MYTZ "CET-1CEST-2,M3.5.0,M10.5.0/3"
+
+
+struct tm tInfo;
+typedef struct structGrowLight
+{
+    int turnOnTime;
+    int turnOffTime;
+    int maxBrightness;
+    int dimTime;
+}growLight;
+
+int getIntTime();
+char minutePassed();
+char secondPassed();
+void initWifi();
+void updateTime(const uint32_t timeout);
+void initTime();
+
+void printTime()
+{
+    char str[30];
+    strftime(str, sizeof(str), "%T", &tInfo);
+    Serial.printf("%s %d\n", str, getIntTime());
+}
+
 void setup()
 {
-    pinMode(LED, OUTPUT);
+    Serial.begin(115200);
+    initWifi();
+    initTime();
 }
 
 void loop()
 {
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-    delay(1000);
+    if(secondPassed())
+    {
+        printTime();
+    }
+}
+
+int getIntTime()
+{
+    char buffer[5];
+    sprintf(buffer, "%2d%2d", tInfo.tm_hour, tInfo.tm_min);
+    return atoi(buffer);
+}
+char minutePassed()
+{
+    time_t now = time(nullptr);
+    localtime_r(&now, &tInfo);
+
+    static uint8_t old;
+    if (tInfo.tm_min != old) {
+        old = tInfo.tm_min;
+        return 1;
+    }
+    return 0;
+}
+char secondPassed()
+{
+    time_t now = time(nullptr);
+    localtime_r(&now, &tInfo);
+
+    static uint8_t old;
+    if (tInfo.tm_sec != old) {
+        old = tInfo.tm_sec;
+        return 1;
+    }
+    return 0;
+}
+void initWifi()
+{
+    Serial.print("Connecting to WiFi");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println(" Connected!");
+}
+void initTime()
+{
+    configTzTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
+    updateTime(5000);
+}
+void updateTime(const uint32_t timeout) {
+    uint32_t start = millis();
+    do {
+        time_t now = time(nullptr);
+        tInfo = *localtime(&now);
+        delay(1);
+    } while (millis() - start < timeout  && tInfo.tm_year <= (1970 - 1900));
 }
