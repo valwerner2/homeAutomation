531c0fe6f783510877c43aa81f49ec90a231e600
Valentin Werner <valwerner2@gmail.com>
2025-04-26 17:54:33 +0200

    html code how has its own file and will be loaded into the indexHtml.h file for easier front end development

--- src/globals.h
+++ src/globals.h
@@ -2,6 +2,10 @@
 // Created by valwe on 08/12/2024.
 //
 
+#ifndef ESP32_PLANTSERVER_GLOBALS_H
+#define ESP32_PLANTSERVER_GLOBALS_H
+
+
 #include "GrowLight.h"
 
 
@@ -12,9 +16,8 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+#include "indexHtml.h"
 
-#ifndef ESP32_PLANTSERVER_HTML_H
-#define ESP32_PLANTSERVER_HTML_H
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -32,41 +35,4 @@ PlantServer::Output PowerBarLeft(4, PlantServer::outputModes::OUTPUT_DIGITAL, HI
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PlantServer</title>
-    <style>
-        html {font-family: Arial; display: inline-block; text-align: center;}
-        h1 {font-size: 2.3rem; color: darkblue}
-        h2 {font-size: 1.5rem; color: darkblue}
-        p {font-size: 1.9rem;}
-        body {max-width: 400px; margin:0px auto; padding-bottom: 25px;}
-        .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: mediumspringgreen;
-            outline: none; -webkit-transition: .2s; transition: opacity .2s;}
-        .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: darkblue; cursor: pointer;}
-        .slider::-moz-range-thumb { width: 35px; height: 35px; background: darkblue; cursor: pointer; }
-    </style>
-</head>
-<body>
-<h1>PlantServer</h1>
-<h2>GrowLights</h2>
-<p><span id="textSliderValue">%SLIDERGROWLIGHT1VALUE%</span></p>
-<p><input type="range" onchange="updateSliderPWM(this)" id="sliderGrowLight1" min="0" max="100" value="%SLIDERGROWLIGHT1VALUE%" step="1" class="slider"></p>
-<script>
-    function updateSliderPWM(element) {
-        var sliderValue = document.getElementById("sliderGrowLight1").value;
-        document.getElementById("textSliderValue").innerHTML = sliderValue;
-        console.log(sliderValue);
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "/growLight1Brightness?value="+sliderValue, true);
-        xhr.send();
-    }
-</script>
-</body>
-</html>
-)rawliteral";
-
-#endif //ESP32_PLANTSERVER_HTML_H
+#endif //ESP32_PLANTSERVER_GLOBALS_H
