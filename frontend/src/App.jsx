148f9eb7b372e485c02d952309c0f7656d505131
Valentin <valwerner2@gmail.com>
2025-07-18 17:29:18 +0200

    then websockets updates devices, update will be presented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
 import PlantServer from "./PlantServer.jsx";
+import {useEffect, useState} from "react";
 
 function App() {
 
+  const deviceState = useState();
+  const [devices, setDevices] = deviceState;
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://127.0.0.1:8080/homeAutomation/ws/deviceTracker");
+
+    ws.onopen = () => {
+      console.log("connected");
+    }
+
+    ws.onmessage = (event) => {
+      setDevices(JSON.parse(event.data));
+      console.log("Update:", JSON.parse(event.data));
+    };
+  }, [])
+
   return (
     <div>
-    <PlantServer />
+    <PlantServer devices={devices}/>
     </div>
   )
 }
