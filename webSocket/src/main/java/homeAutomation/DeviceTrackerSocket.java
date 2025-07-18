41dcf45b29949b546834b6de494158f8ef2472ad
Valentin <valwerner2@gmail.com>
2025-07-18 15:14:59 +0200

    web socket that receives devices info

---
+++ webSocket/src/main/java/homeAutomation/DeviceTrackerSocket.java
@@ -0,0 +1,59 @@
+package homeAutomation;
+
+import com.google.gson.Gson;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static java.util.Objects.requireNonNull;
+
+@ServerEndpoint("/homeAutomation/ws/deviceTracker")
+@ApplicationScoped
+public class DeviceTrackerSocket {
+
+    List<Session> sessions = new CopyOnWriteArrayList<>();
+    Map<String, Device> devices = new ConcurrentHashMap<>();
+    Gson gson = new Gson();
+
+    @OnOpen
+    public void onOpen(Session session)
+    {
+        System.out.println("onOpen>");
+        sessions.add(session);
+    }
+
+    @OnClose
+    public void onClose(Session session)
+    {
+        System.out.println("onClose>");
+        sessions.remove(session);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable)
+    {
+        System.out.println("onError>");
+        throwable.printStackTrace();
+        sessions.remove(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message)
+    {
+        System.out.print("onMessage>");
+
+        Device device = gson.fromJson(message, Device.class);
+
+        System.out.print(device + "\n");
+    }
+}
