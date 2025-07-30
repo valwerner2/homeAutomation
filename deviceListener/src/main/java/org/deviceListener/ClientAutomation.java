a8c71d154d9fcc3c8281a363e9a54f4c50f22032
Valentin <valwerner2@gmail.com>
2025-07-30 14:04:51 +0200

    added runner that executes automations tasks every 5 seconds

--- deviceListener/src/main/java/org/deviceListener/ClientAutomation.java
+++ deviceListener/src/main/java/org/deviceListener/ClientAutomation.java
@@ -1,11 +1,21 @@
 package org.deviceListener;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import jakarta.annotation.PostConstruct;
 import jakarta.websocket.*;
 
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @ClientEndpoint
 public class ClientAutomation
@@ -13,12 +23,20 @@ public class ClientAutomation
     private DeviceData deviceData;
     private Session session;
     private String uri;
+    private List<AutomationTask> automations = new CopyOnWriteArrayList<>();
 
     ClientAutomation(String uri, DeviceData  deviceData)
     {
         this.uri = uri;
         this.deviceData = deviceData;
+
+        AutomationRunner runner = new AutomationRunner(deviceData, automations);
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        // Run every 5 seconds
+        scheduler.scheduleAtFixedRate(runner, 0, 5, TimeUnit.SECONDS);
     }
+
 
     @OnOpen
     public void onOpen(Session session) {
@@ -40,24 +58,13 @@ public class ClientAutomation
     public void onMessage(String message)
     {
         System.out.println("Message from '" + uri + "' :" + message);
-    }
-
-    private void sendHttpRequest(String url) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
-
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenAccept(response -> {
-                    System.out.println("HTTP response status: " + response.statusCode());
-                    System.out.println("HTTP response body: " + response.body());
-                })
-                .exceptionally(e -> {
-                    System.err.println("HTTP request failed: " + e.getMessage());
-                    return null;
-                });
+        Type deviceListType = new TypeToken<List<AutomationTask>>() {}.getType();
+        automations.clear();
+        List<AutomationTask> temp = new Gson().fromJson(message, deviceListType);
+        for(AutomationTask task : temp)
+        {
+            automations.add(task);
+        }
     }
 
     public void connect() {
