2bcf7efbba067058154d8056cb65efab61b1d458
Valentin <valwerner2@gmail.com>
2025-07-30 14:03:19 +0200

    new fields added to AutomationInfo

--- webSocket/src/main/java/homeAutomation/AutomationInfo.java
+++ webSocket/src/main/java/homeAutomation/AutomationInfo.java
@@ -1,11 +1,14 @@
 package homeAutomation;
 
+import java.util.Map;
+
 public class AutomationInfo
 {
     String command;
     String type;
     String macSensor;
     String macSlave;
+    String sensorEndpoint;
     String slaveEndpoint;
-    String targetValue;
+    Map<String, String> properties;
 }
