67cb30041cac10e6ecd255f6f7f57d291b0ec516
Valentin Werner <valwerner2@gmail.com>
2025-08-01 15:43:33 +0200

    timer

--- iosApp/iosApp/DashboardViewModel.swift
+++ iosApp/iosApp/DashboardViewModel.swift
@@ -9,11 +9,12 @@ import Combine
 
 class DashboardViewModel: ObservableObject
 {
-    
     @Published var groupedDevices: [String: [DeviceModel]] = [:]
     
     private var model: DevicesWebSocketModel
     private var cancellables = Set<AnyCancellable>()
+    
+    private var timerCancellable: AnyCancellable?
     
     init(model: DevicesWebSocketModel = DevicesWebSocketModel()) {
         self.model = model
@@ -21,5 +22,25 @@ class DashboardViewModel: ObservableObject
         model.$groupedDevices
             .receive(on: DispatchQueue.main)
             .assign(to: &$groupedDevices)
+        
+        startTimer()
     }
+        
+        private func startTimer() {
+            timerCancellable = Timer
+                .publish(every: 5, on: .main, in: .common)
+                .autoconnect()
+                .sink { [weak self] _ in
+                    self?.pollDevices()
+                }
+        }
+
+        private func pollDevices() {
+            //TODO: send request to get info from devices
+        }
+        
+        deinit {
+            timerCancellable?.cancel()
+        }
+    
 }
